c60a62acd621431456a5b742ac86ab38eee2dde9
Kvana Mac Pro 2 <kvanamacpro2@sunils-macbook-pro.local>
2015-10-01 14:50:20 +0530

    Initial Commit

---
+++ SlideNavigation-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideNavigation-Swift
+//
+//  Created by Kvana Mac Pro 2 on 10/1/15.
+//  Copyright © 2015 Kvana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SlideNavigation-SwiftTests/SlideNavigation_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  SlideNavigation_SwiftTests.swift
+//  SlideNavigation-SwiftTests
+//
+//  Created by Kvana Mac Pro 2 on 10/1/15.
+//  Copyright © 2015 Kvana. All rights reserved.
+//
+
+import XCTest
+@testable import SlideNavigation_Swift
+
+class SlideNavigation_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
